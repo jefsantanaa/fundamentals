9ba3099f81e2c196b777e814c71eada0badb6eb1
Jefferson Santana <jeffersonsantana1914@outlook.com>
2023-12-18 13:35:05 -0300

    New knowledges
    I learned about blocks.

---
+++ start/block.js
@@ -0,0 +1,11 @@
+{ /* To declare a block you must use a brace "{}", a block is a way of grouping 
+  one or more statements or declarations in a program.
+  */
+ 
+  const x = 10; // Declaration of a variable x.
+  console.log(x); // Statament that prints x.
+  if (x > 5) { // Statament that checks a condition.
+    console.log("Passed by here!"); // Statament that prints a message.
+  }
+
+}
---
+++ start/challenge_01.js
@@ -0,0 +1,20 @@
+/*
+  Print all even numbers from 1 to 10 and odd numbers from 1 to 10.
+  Using the concept of blocks.
+*/
+{
+  console.log("Odd numbers");
+  console.log("1");
+  console.log("3");
+  console.log("5");
+  console.log("7");
+  console.log("9");
+}
+{
+  console.log("Even numbers");
+  console.log("2");
+  console.log("4");
+  console.log("6");
+  console.log("8");
+  console.log("10");
+}
