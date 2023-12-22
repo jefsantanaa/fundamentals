000e1a77a5dfdc8af603a9a815efefd55cbb5f95
Jefferson Santana <jeffersonsantana1914@outlook.com>
2023-12-22 15:00:23 -0300

    Upated string

--- start/strings.js
+++ start/strings.js
@@ -1,8 +1,12 @@
-const school = "Cod3r";
+const schoolName = "Cod3r";
 
-console.log(school.charAt(4));
-console.log(school.charAt(5));
-console.log(school.charCodeAt(3));
-console.log(school.indexOf('3'));
-console.log(school.substring(1));
-console.log(school.substring(0, 3));
+console.log(schoolName.charAt(4));
+console.log(schoolName.charAt(5));
+console.log(schoolName.charCodeAt(3));
+console.log(schoolName.indexOf('3'));
+
+console.log(schoolName.substring(1));
+console.log(schoolName.substring(0, 3));
+
+console.log((schoolName).concat(' School').concat('!'));
+console.log(schoolName.replace(3, 'e'));
